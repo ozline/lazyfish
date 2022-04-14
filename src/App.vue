ef6c3251a23cb70361a89e41cee673797f9f202d
OZLIINEX <elicpx@gmail.com>
2022-04-14 23:13:39 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <login></login>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 import Login from './views/login.vue'
 
 export default {
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,25 +1,38 @@
 <template>
-<div style="margin:30px;">
-    <h2>用户登录</h2>
-    <div class="form-floating mb-3" style="margin-top:30px;">
-        <input class="form-control" placeholder="username" v-model="username">
-        <label for="username">用户名</label>
+<div>
+    <div style="margin:30px;">
+        <h2>登录</h2>
+        <div class="form-floating g-2" style="margin-top:26px;">
+            <input class="form-control" placeholder="学号" v-model="username">
+            <label for="username">用户名</label>
+        </div>
         <div class="form-text">注册时使用的用户名</div>
-    </div>
-    <div class="form-floating mb-3">
-        <input type="password" class="form-control" placeholder="passwd" v-model="password">
-        <label for="passwd">密码</label>
+        <div class="form-floating" style="margin-top:26px;">
+            <input type="password" class="form-control" placeholder="passwd" v-model="password">
+            <label for="passwd">密码</label>
+        </div>
         <div class="form-text">用作登录的密码</div>
+        <div class="row g-2 align-items-center" style="margin-top:26px;">
+            <div class="col">
+                <div class="form-floating">
+                    <input class="form-control" placeholder="验证码" v-model="verifycode">
+                    <label for="passwd">验证码</label>
+                </div>
+            </div>
+            <div class="col-auto">
+                <div style="float:right;">
+                    <img v-bind:src="'/cors/plus/verifycode.asp?n='+num" style="width:200px;height:58px;" @click="this.num=Math.random();">
+                </div>
+            </div>
+            <div class="form-text">点击图片刷新</div>
+        </div>
+        <!-- <div class="form-text">{{ localcookie }}</div> -->
+        <div class="d-grid gap-2">
+            <button type="button" class="btn btn-primary" @click="login" style="margin-top:36px;height:48px;">登录</button>
+        </div>
+        <!-- <button type="submit" class="btn btn-outline-danger btn-lg" @click="getCookie" style="margin-left:20px;margin-top:48px;">刷新Cookie</button> -->
+        <!-- <router-link to="/user/register" class="btn btn-outline-danger btn-lg" style="margin-left:20px;">注册</router-link> -->
     </div>
-    <div class="form-floating mb-3">
-        <input class="form-control" placeholder="veryficode" v-model="verifycode">
-        <label for="passwd">验证码</label>
-        <div class="form-text">点击图片即可刷新</div>
-        <img v-bind:src="'/cors/plus/verifycode.asp?n='+num" style="height:35px;width:250px;" @click="this.num=Math.random()">
-    </div>
-    <button type="submit" class="btn btn-outline-primary btn-lg" @click="login">登录</button>
-    <!-- <router-link to="/user/register" class="btn btn-outline-danger btn-lg" style="margin-left:20px;">注册</router-link> -->
-
     <!-- Modal -->
     <div class="modal fade" id="modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog">
@@ -50,10 +63,14 @@ export default {
             verifycode: '',
             msg: "NULL",
             msgTitle: "NULL",
-            num:0
+            num:0,
+            localcookie: document.cookie,
         }
     },
     methods: {
+        getCookie(){
+            this.localcookie=document.cookie
+        },
         login(){
             var data = "muser="+this.username+"&passwd="+this.password+"&Verifycode="+this.verifycode
             this.axios.post("/cors/logincheck.asp",data).then((res)=>{
@@ -66,4 +83,9 @@ export default {
 }
 </script>
 
-
+<style>
+    /* ::placeholder{
+        font-size:14px;
+        line-height: 30px;
+    } */
+</style>
